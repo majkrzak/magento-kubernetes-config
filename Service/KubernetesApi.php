de6456516f70659def75f1b6d85697e6728ad312
Piotr Majkrzak <piotr@majkrzak.dev>
2025-07-27 22:13:36 +0200

    Traverse parents

--- Service/KubernetesApi.php
+++ Service/KubernetesApi.php
@@ -41,7 +41,8 @@ class KubernetesApi
     }
 
     /**
-     * Fetch `magento.config/*` annotations of current Pod and converts them to ConfigEntries.
+     * Fetch `magento.config/*` annotations of current Pod and
+     *  all its ancestors and converts them to ConfigEntries.
      */
     public function parseAnnotations(): \Generator
     {
@@ -49,10 +50,44 @@ class KubernetesApi
             return;
         }
 
-        foreach (((($this->client->request("/api/v1/namespaces/{$this->podNamespace}/pods/{$this->podName}") ?: []) ["metadata"] ?? []) ["annotations"] ?? []) as $key => $val) {
-            if (\str_starts_with($key, self::ANNOTATION_PREFIX)) {
-                yield ConfigEntry::from($key, $val);
+        $f = function (
+            string $apiVersion,
+            string $kind,
+            string $name,
+        ) use (&$f): \Generator {
+
+            $scope = !\str_contains($apiVersion, "/") ? "api" : "apis";
+
+            $plural = \array_values(
+                \array_filter(
+                    $this->client->request("/{$scope}/{$apiVersion}")["resources"],
+                    function ($resource) use ($kind) {
+                        return $resource["kind"] == $kind && !\str_contains($resource["name"], "/");
+                    }
+                )
+            )[0]["name"];
+
+            $data = $this->client->request("/{$scope}/{$apiVersion}/namespaces/{$this->podNamespace}/{$plural}/{$name}");
+
+            foreach ((($data["metadata"] ?? []) ["annotations"] ?? []) as $key => $val) {
+                if (\str_starts_with($key, KubernetesApi::ANNOTATION_PREFIX)) {
+                    yield ConfigEntry::from($key, $val);
+                }
             }
-        }
+
+            foreach ((($data["metadata"] ?? []) ["ownerReferences"] ?? []) as $ownerReference) {
+                yield from $f(
+                    $ownerReference["apiVersion"],
+                    $ownerReference["kind"],
+                    $ownerReference["name"],
+                );
+            }
+        };
+
+        yield from $f(
+            "v1",
+            "Pod",
+            $this->podName,
+        );
     }
 }
